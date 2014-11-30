c749ebacf472b7bda38fdcd01560b11aed9a8d49
StrongLeo <gaopleo@gmail.com>
2014-11-30 12:34:21 -0500

    add footer by Leo

---
+++ src/include/footer.php
@@ -0,0 +1,46 @@
+<div class="footerbox" id='footer'>
+	<div class="infobox">
+		<div class="box1">
+			<h1>Opening Hours</h1>
+			<p>Mon: 11:00am - 11:00pm</p>
+			<p>Tue: 11:00am - 11:00pm</p>
+			<p>Wed: 11:00am - 11:00pm</p>
+			<p>Thu: 11:00am - 11:00pm</p>
+			<p>Fri: 11:00am - 11:00pm</p>
+			<p>Sat: 11:00am - 11:00pm</p>
+			<p>Sun: 11:00am - 11:00pm</p>
+		</div>
+		<span class="line"></span>
+		<div class="box2">
+			<h1>Contact Details</h1>
+			<p>500 Terry Francois Street San Francisco, CA 94158</p>
+			<p>Tel: 123-456-7890</p>
+			<p>Fax: 123-456-7890</p>
+		</div>
+		<span class="line"></span>
+		<div class="box3">
+			<h1>Accept Delivery</h1>
+			<p>Lunch: 11:00am - 4:30pm</p>
+			<p>Dinner: 4:30pm - 10:30pm</p>
+			<br />
+			<p>Happy Hour:</p>
+			<p>Mon-Wed 3:00pm - 7:00pm</p>
+		</div>
+	</div>
+	<div class="copyrightbox">
+		<span>&copy; 2023 by Delicious. Proudly created with <a href="www.7thclass.com">7thclass.com</a></span>
+		<span><a href="#"><img src="" alt="" ></a></span>
+		<span><a href="#"><img src="" alt="" ></a></span>
+		<span><a href="#"><img src="" alt="" ></a></span>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
