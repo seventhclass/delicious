a6aab6805baea1e00e26599573632ecb044546f7
Wang Qi <wangqicanada@gmail.com>
2014-12-04 11:32:22 -0500

    home page slider add by wang qi

--- src/include/home.php
+++ src/include/home.php
@@ -1,12 +1,20 @@
-<figure id="slider">
-	<div class="container">
-		<img src="./images/slider1.jpg" alt="Lorem ipsum dolor sit amet...">
-		<!--<img src="./images/slider2.jpg" alt="Lorem ipsum dolor sit amet...">
-		<img src="./images/slider3.jpg" alt="Lorem ipsum dolor sit amet...">
-		<img src="./images/slider4.jpg" alt="Lorem ipsum dolor sit amet...">-->
-	</div>
-	<figcaption></figcaption> <!-- slideshow caption -->
-	<nav id="slider-nav"></nav> <!-- navigation -->
-</figure>
-<!--<script class="cssdeck" src="./js/jquery.min.js"></script>
-<script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>-->
+<section class="content" id="content_home">
+	 <div id="list" style="left: -980px;">
+		   <img src="./images/5.jpg" alt="5" height="490" width="980"/>
+		   <img src="./images/1.jpg" alt="1" height="490" width="980"/>
+		   <img src="./images/2.jpg" alt="2" height="490" width="980"/>
+		   <img src="./images/3.jpg" alt="3" height="490" width="980"/>
+		   <img src="./images/4.jpg" alt="4" height="490" width="980"/>
+		   <img src="./images/5.jpg" alt="5" height="490" width="980"/>
+		   <img src="./images/1.jpg" alt="1" height="490" width="980"/>			 
+	 </div> 
+	 <div id="buttons">
+	 <span index="1" class="on"></span>
+	 <span index="2" ></span>
+	 <span index="3" ></span>
+	 <span index="4" ></span>
+	 <span index="5" ></span>
+	 </div>	
+	 <a href="javascript:;" class="arrow" id="prev">&lt;</a>			 
+	 <a href="javascript:;" class="arrow" id="next">&gt;</a>
+</section>
