fb1a2f23d3fb8522670814b8d702145b3227dc81
veho <pwh.veronique@gmail.com>
2014-12-05 23:54:23 -0500

    DB design modified

--- Design/DB/Delicious.sql
+++ Design/DB/Delicious.sql
@@ -3,14 +3,11 @@ USE delicious;
 
 /*drop tables*/
 DROP TABLE IF EXISTS resto_admin;
-DROP TABLE IF EXISTS cate_cn;
-DROP TABLE IF EXISTS cate_en;
-DROP TABLE IF EXISTS cate_fr;
+DROP TABLE IF EXISTS cate;
 DROP TABLE IF EXISTS dish;
-DROP TABLE IF EXISTS dish_cn;
-DROP TABLE IF EXISTS dish_en;
-DROP TABLE IF EXISTS dish_fr;
 DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS promotion;
+DROP TABLE IF EXISTS slider;
 
 /*admin*/
 CREATE TABLE resto_admin ( 
@@ -20,22 +17,12 @@ CREATE TABLE resto_admin (
  email varchar(60) not null
 );
 
-/*category-CHINESE*/
-CREATE TABLE cate_cn (
+/*category*/
+CREATE TABLE cate (
  cate_id smallint unsigned auto_increment key,
- cate_name varchar(50) not null unique 
-);
-
-/*category-ENGLISH*/
-CREATE TABLE cate_en (
- cate_id smallint unsigned auto_increment key,
- cate_name varchar(50) not null unique 
-);
-
-/*category-FRENCH*/
-CREATE TABLE cate_fr (
- cate_id smallint unsigned auto_increment key,
- cate_name varchar(50) not null unique 
+ name_cn varchar(50) unique, 
+ name_en varchar(50) unique,
+ name_fr varchar(50) unique 
 );
 
 /*dish*/
@@ -49,29 +36,14 @@ CREATE TABLE dish (
  dish_time int unsigned not null,
  is_show tinyint(1) default 1,
  is_hot tinyint(1) default 0
+ name_cn varchar(50) unique,
+ name_en varchar(50) unique,
+ name_fr varchar(50) unique,
+ desc_cn mediumtext,
+ desc_en mediumtext,
+ desc_fr mediumtext
 );
 
-/*dish-CHINESE*/
-CREATE TABLE dish_cn (
- dish_id int unsigned auto_increment key, 
- dish_name varchar(50) not null unique,
- dish_desc mediumtext
- );
- 
-/*dish-ENGLISH*/
-CREATE TABLE dish_en (
- dish_id int unsigned auto_increment key, 
- dish_name varchar(50) not null unique,
- dish_desc mediumtext
- );
- 
-/*dish-FRENCH*/
-CREATE TABLE dish_fr (
- dish_id int unsigned auto_increment key, 
- dish_name varchar(50) not null unique,
- dish_desc mediumtext
- );
- 
 /*-- user
 DROP TABLE IF EXISTS imooc_user;
 CREATE TABLE imooc_user (    
@@ -87,11 +59,32 @@ CREATE TABLE imooc_user (
 
 /*album*/
 CREATE TABLE album (
- id int unsigned auto_increment key,
+ album_id int unsigned auto_increment key,
  dish_id int unsigned not null,
  album_path varchar(50) not null
 );
 
+/*promotion*/
+CREATE TABLE promotion (
+ prom_id int unsigned auto_increment key,
+ start_time int unsigned,
+ end_time int unsigned not null,
+ title_cn varchar(50),
+ title_en varchar(50),
+ title_fr varchar(50),
+ content_cn mediumtext,
+ content_en mediumtext,
+ content_fr mediumtext,
+ dish_id int unsigned,
+ img_path varchar(50)
+);
+
+/*slider*/
+CREATE TABLE slider (
+ slide_id int unsigned auto_increment key,
+ dish_id int unsigned not null,
+ img_path varchar(50) not null
+);
 /*insert resto_admin (username,password, email) values('admin','21232f297a57a5a743894a0e4a801fc3','admin@qq.com');*/
 
 commit;
