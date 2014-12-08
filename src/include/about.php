dd268c9f913958daa49038e9cf06d03f07adccac
Wang Qi <wangqicanada@gmail.com>
2014-12-08 00:02:49 -0500

    test of about page by wang qi

--- src/include/about.php
+++ src/include/about.php
@@ -1,3 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div {
+display: block;
+}
+s6 { 
+position: absolute;
+top: 107px;
+width: 854px;
+left: 35px;
+
+word-wrap: break-word;
+}
+.s6 h2
+
+.s17 {
+border-bottom: 2px dotted rgba(227, 235, 182, 1);
+height: 0 !important;
+min-height: 0 !important;
+}
+
+</style>
+</head>
+
+
 <section class="content" id="content_about">
-	<div>This is about page<div>
+	<div>
+	
+	<div style="position: absolute; top: 107px; width: 854px; left: 35px;" class="s6" id="WRchTxt1k" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$WRchTxt1k">
+	<h2 style="line-height: 1.4em; text-align: justify;" class="font_2"><span style="line-height: 1.4em;">ABOUT</span></h2>
+
+	
+	
+				
+			<img src="./images/5.jpg" alt="5" height="490" width="910"/>
+			
+	
+</div>
+
+<div style="position: absolute; top: 651px; height: 5px; width: 911px; left: 35px;" class="s17" id="FvGrdLn8" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$FvGrdLn8">
+<div id="FvGrdLn8line" class="s17line" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$FvGrdLn8.0"><hr/></div></div>
+
+
+<div style="position: absolute; top: 724px; width: 263px; left: 52px;" class="s6" id="WRchTxt5" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$WRchTxt5">
+<h6 style="line-height: 1.4em;" class="font_6"><span style="line-height: 1.4em;">I'm a title.</span></h6>
+
+<h6 style="line-height: 1.4em;" class="font_6"><span style="line-height: 1.4em;">Click here to add your own text and edit me.</span></h6>
+
+
+
+
+<div style="position: absolute; top: 0px; width: 580px; left: 345px;" class="s6" id="WRchTxt1l" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$WRchTxt1l"><p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. </span></p>
+
+<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">​</span></p>
+
+<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">I’m a great place for you to tell a story and let your users know a little more about you.&nbsp;This is a great space to write long text about your company and your services. </span></p>
+
+<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">​</span></p>
+
+<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">Tip: Add your own image by double clicking the image and clicking Change Image</span></p>
+</div>
+
+
+</div>
+
+
+     
+	
+	
+	
+	
+	
+	
+	
+	</div>
 </section>
+</html>
