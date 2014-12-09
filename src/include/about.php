db3ea17f67299e3ba103de16e1cb42a7fdc3a0ed
Wang Qi <wangqicanada@gmail.com>
2014-12-09 00:56:04 -0500

    modify about page by wang qi

--- src/include/about.php
+++ src/include/about.php
@@ -11,9 +11,22 @@ top: 107px;
 width: 854px;
 left: 35px;
 
+
 word-wrap: break-word;
 }
-.s6 h2
+#about1{color:blue;
+    font-size:60px;}
+#about2{color:blue;
+    font-size:30px;}
+#about3{color:blue;
+    font-size:30px;}
+#about4{color:blue;
+    font-size:15px;}
+#about5{color:blue;
+    font-size:15px;}
+#about6{color:blue;
+    font-size:15px;}
+
 
 .s17 {
 border-bottom: 2px dotted rgba(227, 235, 182, 1);
@@ -29,7 +42,9 @@ min-height: 0 !important;
 	<div>
 	
 	<div style="position: absolute; top: 107px; width: 854px; left: 35px;" class="s6" id="WRchTxt1k" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$WRchTxt1k">
-	<h2 style="line-height: 1.4em; text-align: justify;" class="font_2"><span style="line-height: 1.4em;">ABOUT</span></h2>
+	
+	<h2 ><span id="about1">关于我们</span></h2>
+	<br/>
 
 	
 	
@@ -39,27 +54,30 @@ min-height: 0 !important;
 	
 </div>
 
-<div style="position: absolute; top: 651px; height: 5px; width: 911px; left: 35px;" class="s17" id="FvGrdLn8" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$FvGrdLn8">
+<div style="position: absolute; top: 671px; height: 5px; width: 911px; left: 35px;" class="s17" id="FvGrdLn8" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$FvGrdLn8">
 <div id="FvGrdLn8line" class="s17line" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$FvGrdLn8.0"><hr/></div></div>
 
 
 <div style="position: absolute; top: 724px; width: 263px; left: 52px;" class="s6" id="WRchTxt5" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$WRchTxt5">
-<h6 style="line-height: 1.4em;" class="font_6"><span style="line-height: 1.4em;">I'm a title.</span></h6>
-
-<h6 style="line-height: 1.4em;" class="font_6"><span style="line-height: 1.4em;">Click here to add your own text and edit me.</span></h6>
-
-
+<p ><span id="about2">我们比较小.</span></p>
+<br/>
+<p ><span id="about3">我们成立于2014年.</span></p>
 
 
-<div style="position: absolute; top: 0px; width: 580px; left: 345px;" class="s6" id="WRchTxt1l" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$WRchTxt1l"><p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. </span></p>
 
-<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">​</span></p>
 
-<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">I’m a great place for you to tell a story and let your users know a little more about you.&nbsp;This is a great space to write long text about your company and your services. </span></p>
 
-<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">​</span></p>
 
-<p style="line-height: 1.4em;" class="font_8"><span style="line-height: 1.4em;">Tip: Add your own image by double clicking the image and clicking Change Image</span></p>
+<div style="position: absolute; top: 0px; width: 580px; left: 345px;" class="s6" id="WRchTxt1l" data-reactid=".0.$SITE_ROOT.$SITE_STRUCTURE.$PAGES_CONTAINER.1.1.$SITE_PAGES.$c42f.1.$WRchTxt1l">
+<p ><span id="about4">ontent and make changes to the font. Feel free to drag and drop me anywhere you like on your page. </span></p>
+   <br/>
+
+<p ><span id="about5">​I’m a great place for you to tell a story and let your</span></p>
+<br/>
+
+<p ><span id="about6">I’m a great place for you to tell a story and let your users know a little more about you.&nbsp;This is a great space to write long text about your company and your services. </span></p>
+<br/>
+
 </div>
 
 
