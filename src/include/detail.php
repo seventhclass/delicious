b252e9aa8850349534f6e1b6e7d50672a18335ae
veho <pwh.veronique@gmail.com>
2014-12-25 19:05:31 -0500

    popup page modified by veho

--- src/include/detail.php
+++ src/include/detail.php
@@ -1,28 +1,58 @@
 <div id="page_overlay"></div>
 <div id="popup">
-	<div id="popup_nav">
-		<input id="back" type="image" src="./images/arrow-back.png">
-		<input id="forward" type="image" src="./images/arrow-forward.png">
-	</div>
-	<div id="popup_page">
-		<input id="close"  type="image" src="./images/close-r.png">
-		<!--<div id="dish_pic">
-			<div id="pic_large"><img src="" style="opacity: 1;"></div>
-			<hr/>
-			<div>thumb nail images go here</div>
-		</div>
-		
-		<div>
-			
-		</div>
-		<div></div>
-		-->
-		
-	</div>
 	
+	<!--<div id="popup_nav">
+		<input id="back"type="image" src="arrow-back-white.png">
+		<input id="forward"type="image" src="arrow-forward-white.png">
+	</div>-->
+	<div id="popup_nav">
+		<div id="back"><a href="javascript:;"></a></div>
+		<div id="forward"><a href="javascript:;"></a></div>
+	</div>
+	<div id="popup_main">
+		<!--<div id="popup_topbar">
+			<p>
+				<input id="close"  type="image" src="gray-close-circled-32-1.png">
+			</p>
+		</div>-->
+		<div id="close"><a href="javascript:;"></a></div>
+		<div id="popup_page">
+		<div id="dish_pic">
+			<div id="pic_large"><img src="" style="opacity: 1;"></div>
+			<div id="pic_small">thumb nail images go here</div>
+		</div>
+		
+		<div id="dish_text">
+			<h1>Dish Name Dish Name Dish Name Dish Name</h1>
+			<ul>
+				<li><div class="dt">Dish No.</div>
+					<div class="dd">10001</div>
+				</li>
+				<li><div class="dt">Category:</div>
+					<div class="dd">Cantonese and Hong Kong Style</div>
+				</li>
+				<li><div class="dt">Spicy:</div>
+					<div class="dd" id="is_spicy"></div>
+				</li>
+				<li><div class="dt">Current Price:</div>
+					<div class="dd">$9.99</div>
+				</li>
+				<li><div class="dt">Regular Price:</div>
+					<div class="dd">$11.99</div>
+				</li>
+			</ul>
+		</div>
+		
+		<div id="dish_desc">
+			<h1>Description</h1>
+		</div>
+		
+	</div>
 	
 	
 	<div id="popup_footer">
 		<p><span id="slideNumber">4</span> / <span id="slideTotal">14<span></p>
+	</div>
+	
 	</div>
 </div>
