455363dff697e7419eb7bdb6b9141082e09faf64
Wang Qi <wangqicanada@gmail.com>
2015-01-08 23:54:34 -0500

    about icon by wang qi

--- src/include/about.php
+++ src/include/about.php
@@ -15,10 +15,20 @@
 			<p>H3T xxx<p>
 			<p>Tel: 514-342-7687</p>
 		</div>
+		
 		<div  class="right">
 			<span>Lorem ipsum dolor sit amet, id quo fabellas comprehensam. Duo in atqui ceteros quaestio. Animal perfecto ei cum. In eam libris perfecto lobortis, eam doming denique expetenda eu, et tempor inciderint mel. At eum falli prodesset gloriatur. Mei ex erant neglegentur, cu pro consul altera.
 
 An pri viderer omittam suscipit, in mei quas adolescens. Vix vivendo offendit id, eam ex clita petentium, qui reque constituto ullamcorper et. An nec tempor urbanitas vituperata, labitur delicatissimi ne mei, no enim graecis reprehendunt mea. Tincidunt quaerendum vel in, per ea putant doctus.</span>
+		</div>
+		<div  class="left_icon">
+			
+			<a href="http://www.facebook.com"><img src="./images/facebook.png"  alt="facebook"  height="40" width="40"/></a>
+			<a  href="http://www.apple.com"><img src="./images/apple.png" alt="apple"  height="40" width="40"/></a>
+			<a  href="http://www.google.com"><img src="./images/googleplus.png" alt="google"  height="40" width="40"/></a>
+			<a  href="http://www.paypal.com"><img src="./images/paypal.png" alt="paypal"  height="40" width="40"/></a>
+			<a  href="http://www.skype.com"><img src="./images/skype.png" alt="skype"  height="40" width="40"/></a>
+			<a  href="http://www.twitter.com"><img src="./images/twitter.png" alt="twitter"  height="40" width="40"/></a>
 		</div>
 	</div>
 </section>
