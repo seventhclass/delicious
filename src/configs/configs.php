2f7b4e887ec41eb7ffda3b28f0ff3cb74c9bfac5
veho <pwh.veronique@gmail.com>
2015-01-10 23:02:52 -0500

    merge before push veho

--- src/configs/configs.php
+++ src/configs/configs.php
@@ -3,24 +3,16 @@
 define("DEVELOPMENT_MODE","dev");	//Development Mode
 //define("DEVELOPMENT_MODE","rel");	//Release Mode
 
-//define("DEVELOPMENT_MODE","c9");	//Development by c9 env
-define("DB_HOST","127.0.0.1");
+define("DB_HOST", "127.0.0.1");
 
-//define("DB_HOST", "localhost");
-
-define("DB_USER", "lehu3000");
-define("DB_PWD", "");
-define("DB_DBNAME", "delicious");
-
-/*
 if(DEVELOPMENT_MODE=="dev"){	
 	define("DB_USER", "root");
 	define("DB_PWD", "root"); 
-	define("DB_DBNAME", "7thclass_delicious");
+	define("DB_DBNAME", "delicious");
 }else{
 	define("DB_USER", "adminjBgZCk8");
 	define("DB_PWD", "xbdwsnmQVYi_");
 	define("DB_DBNAME", "delicious");
 }
-*/
+
 define("DB_CHARSET", "utf8");
